af24ce16b7e990b06ce56c733b515e3dc3dc59c6
FP2003 <filippieleckii@gmail.com>
2024-03-22 19:16:52 +0000

    IntroductionToPandas

--- IntroductionToPandas/DropDuplicateEmails.py
+++ IntroductionToPandas/DropDuplicateEmails.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    customers.drop_duplicates(subset = ['email'], keep = 'first', inplace = Tru)
+    customers.drop_duplicates(subset = ['email'], keep = 'first', inplace = True)
     return customers
