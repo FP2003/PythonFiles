1296b68d202e8790dad071a5579f2adba9fff1e4
FP2003 <filippieleckii@gmail.com>
2024-04-09 00:03:32 +0100

    Leetcode75

--- Leetcode75/LeafSimiliarTrees.py
+++ Leetcode75/LeafSimiliarTrees.py
@@ -1,9 +1,12 @@
+from pyparsing import Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         # Function to find leaf nodes of a binary tree
