5b1c607cc01ea2a3578850b803b3bb787517ac7d
FP2003 <filippieleckii@gmail.com>
2024-05-14 23:43:07 +0100

    TopInterview150

---
+++ TopInterview150/canJump.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def canJump(nums: List[int]) -> bool:
+    # Initialize the target index to the last index of the array
+    target = len(nums) - 1
+    # Start iteration from the second-to-last index of the array
+    i = len(nums) - 2
+    
+    # Iterate backwards through the array
+    while i >= 0:
+        # Check if it's possible to reach the current index 'i' from 'target'
+        if target <= i + nums[i]:
+            # If reachable, update the target index to the current index 'i'
+            target = i
+        # Move to the previous index
+        i -= 1
+    
+    # If the target index reaches the beginning of the array (index 0), return True
+    if target == 0:
+        return True
+    # If not, return False
+    return False
