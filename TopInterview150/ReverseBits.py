ee6d466f55f24bd2f3ef56837213b4eb5b568d24
FP2003 <filippieleckii@gmail.com>
2024-05-24 22:07:51 +0100

    TopInterview150

---
+++ TopInterview150/ReverseBits.py
@@ -0,0 +1,8 @@
+def reverseBits(n: int) -> int:
+        return int("{:032b}".format(n)[::-1], 2)
+    
+# Convert integer n to a 32-bit binary string, reverse the string, and convert it back to an integer
+
+
+
+    
