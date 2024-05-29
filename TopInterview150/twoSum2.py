49382b04076f51927a0bbf47ed1dc54fd89587be
FP2003 <filippieleckii@gmail.com>
2024-05-29 22:15:01 +0100

    TopInterview150

--- TopInterview150/twoSum2.py
+++ TopInterview150/twoSum2.py
@@ -7,15 +7,12 @@ def twoSum(numbers: List[int], target: int) -> List[int]:
 
     # Continue the loop until the two pointers meet
     while i < j:
-        # Calculate the sum of the numbers at the current pointers
-        current_sum = numbers[i] + numbers[j]
-        
         # Check if the current sum matches the target
-        if current_sum == target:
+        if numbers[i] + numbers[j] == target:
             # If the target is found, return the 1-based indices of the two numbers
             return [i + 1, j + 1]
         # If the current sum is less than the target, increment the left pointer i
-        elif current_sum < target:
+        elif numbers[i] + numbers[j] < target:
             i += 1
         # If the current sum is greater than the target, decrement the right pointer j
         else:
